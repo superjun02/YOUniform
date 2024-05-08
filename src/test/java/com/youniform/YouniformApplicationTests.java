365e99f822bbe1e67545eff679f7a436ff6bdca9
DESKTOP-5MMQDH9\이상준 <superjun02@naver.com>
2024-05-08 19:14:41 +0900

    초기 환경 세팅

---
+++ src/test/java/com/youniform/YouniformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youniform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouniformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
